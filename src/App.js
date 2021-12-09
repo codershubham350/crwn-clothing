212ea98a007439300bf35990b018afe7bdfccb52
Shubham Saxena <shubhamsaxena350@gmail.com>
2021-12-09 11:23:17 +0530

    adding header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      {/* Place header outside the <Switch> or <Route> component to fix it's position */}
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -5,7 +5,7 @@ import "./collection-preview.styles.scss";
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className="collection-preview">
-      <h1 clasName="title">{title.toUpperCase()}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
         {items
           .filter((item, idx) => idx < 4)
